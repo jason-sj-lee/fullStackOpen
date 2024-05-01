e1d15831655df4c7dd365b807a615bf0a53483a0
jason-sj-lee <jason.981214@gmail.com>
2024-05-01 16:40:02 -0400

    Part 2: Exercise 2.12 & 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personServices from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    personServices
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
 
   const peopleToShow = persons.filter(person => person.name.toLowerCase().includes(searchName))
   
@@ -49,9 +53,14 @@ const App = () => {
       return
     }
 
-    setPersons(persons.concat(personObj))
-    setNewName('')
-    setNewNumber('')
+    axios
+    .post('http://localhost:3001/persons', personObj)
+    .then(response => {
+      console.log(response)
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
   }
 
   return (
